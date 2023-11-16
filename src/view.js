84c765fdb8f3774dcb3224065523ad4ff77d994f
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-11-15 21:15:23 -0300

    feat: add view

--- src/view.js
+++ src/view.js
@@ -1,15 +1,53 @@
 export default class View {
-  #csvFile = document.querySelector("#csv-file");
-  #fileSize = document.querySelector("#file-size");
+    #fileSize = document.getElementById("file-size");
+    #csvFile = document.getElementById("csv-file");
+    #form = document.getElementById("form");
+    #debug = document.getElementById("debug");
+    #progress = document.getElementById("progress");
+    #worker = document.getElementById("worker");
 
-  setFileSize(size) {
-    this.#fileSize.innerText = `File size: ${size} \n`;
-  }
+    setFileSize(size) {
+        this.#fileSize.innerText = `File Size: ${size}`;
+    }
 
-  configureOnFileChange(fn) {
-    this.#csvFile.addEventListener("change", (e) => {
-      var file = e.target.files[0];
-      fn(file);
-    });
-  }
+    configureOnFileChange(fn) {
+        this.#csvFile.addEventListener("change", (event) => {
+            fn(event.target.files[0])
+        });
+    }
+
+    configureOnFileSubmit(fn) {
+        this.#form.reset();
+        this.#form.addEventListener("submit", (event) => this.getFormData(event, fn));
+    }
+
+    getFormData(event, fn) {
+        event.preventDefault();
+        const file = this.#csvFile.files[0];
+        const form = new FormData(event.currentTarget);
+        const description = form.get("description");
+
+        if (!file) {
+            alert("Please select a file!")
+        }
+
+        this.updateDebugLog("");
+        fn({ description, file })
+    }
+
+    updateDebugLog(text, reset = true) {
+        if (reset) {
+            this.#debug.innerText = text;
+            return;
+        }
+        this.#debug.innerHTML += text;
+    }
+
+    updateProgress(value) {
+        this.#progress.value = value;
+    }
+
+    isWorkerEnabled() {
+        return this.#worker.checked;
+    }
 }
